5340c0e153a35e446a8b0828937fc6846251ffd0
pogist <murilo.paixao.2@gmail.com>
2017-09-19 12:09:34 -0300

    (chore): Added money-rails test helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
 require 'rspec/rails'
+require 'money-rails/test_helpers'
 require 'database_cleaner'
 
 Shoulda::Matchers.configure do |config|
