ecf7ef1a0230d9102d2ae969a2cca0ad8ccf05ba
pogist <murilo.paixao.2@gmail.com>
2017-09-22 15:51:15 -0300

    (chore): Added simple db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'money'
+
+products = (1..10).map do
+  attrs = {
+    description: Faker::StarWars.quote,
+    title:       Faker::StarWars.character,
+    images_url:  Faker::Internet.url,
+    price:       Money.from_amount(rand(25..50), "BRL")
+  }
+
+  Product.create attrs
+end
+
+products.each do |product|
+  attrs = {
+    description: Faker::StarWars.quote,
+    finished_at: 10.days.from_now,
+    min_buyers:  rand(15..25),
+    product:     product
+  }
+
+  Campaign.create attrs
+end
